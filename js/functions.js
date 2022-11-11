4e3ee87114a86c50eb71bad35beab159adf941f9
Andrengr0 <andrenegreirosmoreira@gmail.com>
2022-11-10 22:42:03 -0300

    corrections

--- js/functions.js
+++ js/functions.js
@@ -10,5 +10,32 @@ $(()=>{
             return false;
         })
     })
+
+
+    const colors = ['#AC9100', '#ff0000']
+
+    // animarTitulo();
+    
+    function animarTitulo(){
+        var i = 0;
+        setInterval(function(){
+            if(i == 0){
+                $('.apresentacion h1').css('color',colors[0]);
+                i++;
+            }else{
+                $('.apresentacion h1').css('color',colors[1]);
+                i == 0;
+            }
+        },1500)
+    }
+
+
+
+    var i = 0;
+    setInterval(function(){
+      $('.apresentacion h1').css('color', colors[i]);
+      i = (i == (colors.length -1)) ? 0 : i+1;
+    },1500);
+
     
 })
